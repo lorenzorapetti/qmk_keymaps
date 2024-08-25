86620fe570129bc7d2e65426cab12bcd9d5dc677
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2024-08-25 20:51:00 +0200

    Add typing streak

--- keyboards/boardsource/unicorne/keymaps/lorenzorapetti/config.h
+++ keyboards/boardsource/unicorne/keymaps/lorenzorapetti/config.h
@@ -1,3 +1,6 @@
 /* QMK */
 #define TAPPING_TERM 280
 #define PERMISSIVE_HOLD
+
+/* Achordion */
+#define ACHORDION_STREAK
--- keyboards/boardsource/unicorne/keymaps/lorenzorapetti/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/lorenzorapetti/keymap.c
@@ -207,6 +207,21 @@ bool achordion_chord(uint16_t tap_hold_keycode,
   return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
+uint16_t achordion_streak_chord_timeout(
+    uint16_t tap_hold_keycode, uint16_t next_keycode) {
+  if (IS_QK_LAYER_TAP(tap_hold_keycode)) {
+    return 0;  // Disable streak detection on layer-tap keys.
+  }
+
+  // Otherwise, tap_hold_keycode is a mod-tap key.
+  uint8_t mod = mod_config(QK_MOD_TAP_GET_MODS(tap_hold_keycode));
+  if ((mod & MOD_LSFT) != 0) {
+    return 80;  // A shorter streak timeout for Shift mod-tap keys.
+  } else {
+    return 180;  // A longer timeout otherwise.
+  }
+}
+
 bool process_detected_host_os_kb(os_variant_t detected_os) {
     if (!process_detected_host_os_user(detected_os)) {
         return false;
